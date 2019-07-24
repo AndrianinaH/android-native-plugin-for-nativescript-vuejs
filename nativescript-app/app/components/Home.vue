067edd5034e6f8894110adc2ae22545963ca27c3
Andrianina <andrianina@hairun-technology.com>
2019-07-24 17:48:47 +0300

    use plugin in nativescript

--- nativescript-app/app/components/Home.vue
+++ nativescript-app/app/components/Home.vue
@@ -1,41 +1,48 @@
 <template>
-    <Page class="page">
-        <ActionBar class="action-bar">
-            <Label class="action-bar-title" text="Home"></Label>
-        </ActionBar>
+  <Page class="page">
+    <ActionBar class="action-bar">
+      <Label class="action-bar-title" text="Home"></Label>
+    </ActionBar>
 
-        <GridLayout>
-            <Label class="info" horizontalAlignment="center" verticalAlignment="center">
-                <FormattedString>
-                    <Span class="fa" text.decode="&#xf135; "/>
-                    <Span :text="message"/>
-                </FormattedString>
-            </Label>
-        </GridLayout>
-    </Page>
+    <GridLayout>
+      <Label class="info" horizontalAlignment="center" verticalAlignment="center">
+        <FormattedString>
+          <Span class="fa" text.decode="&#xf135; " />
+          <Span :text="message" />
+          <Button text="Mon plugin Native" @tap="pageLoaded" />
+        </FormattedString>
+      </Label>
+    </GridLayout>
+  </Page>
 </template>
 
 <script>
-    export default {
-        computed: {
-            message() {
-                return "Blank {N}-Vue app";
-            }
-        }
-    };
+export default {
+  computed: {
+    message() {
+      return "Test de mon propre plugin android";
+    },
+    pageLoaded() {
+      var toaster = require("nativescript-test");
+
+      // show the toast as soon as the app loads
+      toaster.showToast();
+    }
+  }
+};
 </script>
 
 <style scoped lang="scss">
-    // Start custom common variables
-    @import '../app-variables';
-    // End custom common variables
+// Start custom common variables
+@import "../app-variables";
+// End custom common variables
 
-    // Custom styles
-    .fa {
-        color: $accent-dark;
-    }
+// Custom styles
+.fa {
+  color: $accent-dark;
+}
 
-    .info {
-        font-size: 20;
-    }
+.info {
+  font-size: 20;
+}
 </style>
